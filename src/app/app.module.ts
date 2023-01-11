04d2d6bb444bc9cd7f2b9ad38161241ebcdbd97a
Andro Ostoic <aostoicm@gmail.com>
2023-01-11 12:36:35 -0300

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavHeaderComponentComponent } from './components/nav-header-component/nav-header-component.component';
 import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
 
 @NgModule({
   declarations: [
@@ -12,8 +15,10 @@ import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component
     HomeLayoutComponent,
     NavHeaderComponentComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAnalytics(() => getAnalytics())],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'open-banking-sitio-publico',
+    appId: '1:837245874931:web:29dae69ccdc44d89c33d86',
+    storageBucket: 'open-banking-sitio-publico.appspot.com',
+    apiKey: 'AIzaSyDGsLG5qRZ3Av910YcCf5OaUikeLd60LWU',
+    authDomain: 'open-banking-sitio-publico.firebaseapp.com',
+    messagingSenderId: '837245874931',
+    measurementId: 'G-NQQNBPJH6V',
+  },
   production: false,
 };
