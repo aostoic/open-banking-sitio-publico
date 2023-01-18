1709a6071224498af06cf017c6126d41ed38804a
mauricioACV <mauricio.campusano@gmail.com>
2023-01-18 11:03:54 -0300

    implementación de ngFor para iterar html repetido

--- src/app/components/need-help-component/need-help-component.component.ts
+++ src/app/components/need-help-component/need-help-component.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./need-help-component.component.scss'],
 })
 export class NeedHelpComponentComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  accordionItems: { title: string, content: string }[] = [
+    {
+      title: "¿Qué es una API?",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem recusandae at alias ullam a. Magni debitis porro deserunt sint fugit, impedit expedita quidem quasi odio adipisci voluptatem perspiciatis cupiditate totam!"
+    },
+    {
+      title: "¿Qué API's tenemos disponibles?",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem recusandae at alias ullam a. Magni debitis porro deserunt sint fugit, impedit expedita quidem quasi odio adipisci voluptatem perspiciatis cupiditate totam!"
+    },
+    {
+      title: "¿Cómo puedo probar las API’s disponibles?",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem recusandae at alias ullam a. Magni debitis porro deserunt sint fugit, impedit expedita quidem quasi odio adipisci voluptatem perspiciatis cupiditate totam!"
+    },
+    {
+      title: "¿Qué es una API abierta y una API cerrada?",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem recusandae at alias ullam a. Magni debitis porro deserunt sint fugit, impedit expedita quidem quasi odio adipisci voluptatem perspiciatis cupiditate totam!"
+    },
+  ]
+
+  constructor() { }
+
+  ngOnInit() { }
 }
